c942e6ba711e94bd5624172a9dbe45142d188159
DanyMustaine2021 <daniel.chaparro@mds.torrens.edu.au>
2021-11-06 16:35:07 +1100

    Ctest does not work

---
+++ build/Tests/tests.cpp
@@ -0,0 +1,39 @@
+#define CATCH_CONFIG_MAIN
+
+#include <iostream>
+
+#include "catch.hpp"
+
+
+TEST_CASE("FindMax") {
+  
+  SECTION("Should find max of consecutive numbers") {
+    std::vector<int> inputs = {1, 2, 3, 4};
+    REQUIRE(cpplib.FindMax(inputs) == 4);
+  }
+
+  SECTION("Should find max of non consecutive numbers") {
+    std::vector<int> inputs = {1, 7, 3, 4};
+    REQUIRE(cpplib.FindMax(inputs) == 7);
+  }
+
+  SECTION("Should find max with negative numbers") {
+    std::vector<int> inputs = {-1, -7, -3, -4};
+    REQUIRE(cpplib.FindMax(inputs) == -1);
+  }
+
+  SECTION("Should find max of all equal values") {
+    std::vector<int> inputs = {-1, -1, -1, -1};
+    REQUIRE(cpplib.FindMax(inputs) == -1);
+  }
+
+  SECTION("Should find return min for empty vector") {
+    std::vector<int> inputs = {};
+    REQUIRE(cpplib.FindMax(inputs) == std::numeric_limits<int>::min());
+  }
+
+  SECTION("Should find return min for empty vector") {
+    std::vector<int> inputs = {1000};
+    REQUIRE(cpplib.FindMax(inputs) == 1000);
+  }
+}
