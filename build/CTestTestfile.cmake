f9de0f237e60bf2e0aa3e08c151ebe8c0c1f28ef
DanyMustaine2021 <daniel.chaparro@mds.torrens.edu.au>
2021-11-06 15:26:38 +1100

    Ctest init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: G:/My Drive/Torrnes University/DevOps Tools/A2-Sets-DOT503
+# Build directory: G:/My Drive/Torrnes University/DevOps Tools/A2-Sets-DOT503/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
